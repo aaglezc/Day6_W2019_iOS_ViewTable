dab1c0c8e5726c4f018485d4ea277aba1eedc6ec
MacStudent <macstudent@126-iMac05.local>
2019-03-09 12:48:28 -0500

    Initial Commit

---
+++ Day6_W2019_iOS_ViewTableTests/Day6_W2019_iOS_ViewTableTests.swift
@@ -0,0 +1,34 @@
+//
+//  Day6_W2019_iOS_ViewTableTests.swift
+//  Day6_W2019_iOS_ViewTableTests
+//
+//  Created by MacStudent on 2019-03-09.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import XCTest
+@testable import Day6_W2019_iOS_ViewTable
+
+class Day6_W2019_iOS_ViewTableTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
